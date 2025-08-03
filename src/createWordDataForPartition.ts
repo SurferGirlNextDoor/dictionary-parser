209820e8ea9dfd2e2e21bf5edbaab0bdb16b8570
Kori Ryter <kryter@gmail.com>
2025-08-03 16:10:06 -0500

    feat: update types to match UI requests (#7)
    Create a wordExport type with the word references baked in.
    
    Make the exported data smaller by reducing unneeded data.
    
    Signed-off-by: Kori Ryter <kryter@gmail.com>

--- src/createWordDataForPartition.ts
+++ src/createWordDataForPartition.ts
@@ -1,67 +1,15 @@
-import { WordData, WordDisplayData, WordDisplayVariant, WordReferenceData } from "./wordDataTypes";
-
-// The spelling 'CONSTRUCTOR' can't be used as an object key is js so we add a prefix to all spellings.
-const spellingPrefix = '#';
+import { WordExportData } from "./wordDataTypes";
 
 export function createWordDataForPartition(wordIdPartition: string[], 
-  wordIdToWord: {[index: string]: WordData}, 
-  wordIdToWordReferenceData: {[index: string]: WordReferenceData}): {
-    spellingToWordIds: {[index: string]: string[]},
-    referenceWords: WordReferenceData[],
-  wordDisplayData: WordDisplayData[]
-} {
-  const spellingToWordIds: {[index: string]: string[]} = {};
-  const referenceWords: WordReferenceData[] = [];
-  const wordDisplayData: WordDisplayData[] = [];
+  wordIdToWord: {[index: string]: WordExportData}): WordExportData[] {
+  const wordExports: WordExportData[] = [];
 
   wordIdPartition.forEach(wordId => {
-    const wordData = wordIdToWord[wordId];
-
-    // Collect all the spellings for the word.
-    wordData.spellings.forEach(spelling => {
-      if (spelling) {
-        const prefixedSpelling = `${spellingPrefix}${spelling}`
-        const wordIds = spellingToWordIds[prefixedSpelling];
-        if (!wordIds) {
-          spellingToWordIds[prefixedSpelling] = [wordId];
-        } else {
-          wordIds.push(wordId);
-        }
-      }
-    });
-
-    // Collect all the word reference data for the word.
-    const wordReferenceData: WordReferenceData = wordIdToWordReferenceData[wordId];
-    if (wordReferenceData) {
-      referenceWords.push(wordReferenceData);
-    }
-
-    // Collect all the display variants for the word.
-    const variants: WordDisplayVariant[] = [];
-    wordData.variants.forEach(variant => {
-      if (variant) {
-        variants.push({
-          rawData: variant.rawData,
-          isArchaic: variant.isArchaic,
-          isObsolete: variant.isObsolete,
-          partsOfSpeech: variant.partsOfSpeech,
-        });
-      }
-    });
+    const wordExport: WordExportData = wordIdToWord[wordId];
 
     // Collect all the display data for the word.
-    wordDisplayData.push({
-      wordId,
-      spellingsString: wordData.spellingsString,
-      variants,
-      hasReferences: !!wordReferenceData
-    });
+    wordExports.push(wordExport);
   });
 
-
-  return {
-    spellingToWordIds,
-    referenceWords,
-    wordDisplayData
-  }
+  return wordExports;
 }
